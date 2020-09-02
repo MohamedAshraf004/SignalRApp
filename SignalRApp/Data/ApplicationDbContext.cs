a651e1ed917d47b87faae2fbd33fec7056befdca
Mohamed Ashraf <LionelMido@students.mans.edu.eg>
2020-09-02 19:15:38 +0200

    Add project files.

---
+++ SignalRApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SignalRApp.Models;
+
+namespace SignalRApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Message>().HasOne(a => a.Sender)
+                .WithMany(m => m.Messages).HasForeignKey(f => f.UserId);
+        }
+    }
+}
